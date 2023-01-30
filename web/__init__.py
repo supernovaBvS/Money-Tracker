4dfe069562b5b0b7e8e7ed38a9bf84c06ee70286
supernovaBvS <kongson14@gmail.com>
2023-01-30 17:30:08 +0800

    built models

--- web/__init__.py
+++ web/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy
+DB_NAME = "transaction.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'idasjifodafg'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/transaction'
+    db.init_app(app)
+    
     from .views import views
     from .auth import auth
 
