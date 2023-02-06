19ef5b67f4423e9e801380c2c927d6dbea370e40
supernovaBvS <kongson14@gmail.com>
2023-02-06 11:25:46 +0800

    database created, can create acc now

--- web/__init__.py
+++ web/__init__.py
@@ -1,20 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-db = SQLAlchemy
+db = SQLAlchemy()
 DB_NAME = "transaction.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'idasjifodafg'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/transaction'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/transaction'
     
     from .views import views
     from .auth import auth
 
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(views, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- web/auth.py
+++ web/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 auth = Blueprint('auth', __name__)
 
@@ -16,20 +20,25 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash("Email must be at least 4 characters.", category= 'error')
-        elif len(firstName) < 2 or len(lastName) < 2:
+        elif len(first_name) < 2 or len(last_name) < 2:
             flash("First and Last name must be at least 2 characters.", category= 'error')
         elif password1 != password2:
             flash("Passwords doesnt match.", category= 'error')
         elif len(password1) < 7:
             flash("Password must be at least 7 characters.", category= 'error')
         else:
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account created!", category= 'success')
+            return redirect(url_for('views.home'))
+
 
     return render_template("sign_up.html")
