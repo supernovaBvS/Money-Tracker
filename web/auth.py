232248b91683cbf84f5039fe25ca78a619fc2577
supernovaBvS <kongson14@gmail.com>
2023-02-07 15:11:15 +0800

    loggin in user

--- web/auth.py
+++ web/auth.py
@@ -8,6 +8,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Invalid password, try again!', category='error')
+        else:
+            flash('Email does not exist, try again!', category='error')
     return render_template("login.html", booleans=True)
 
 
@@ -25,6 +38,9 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email is already registered.', category='error')
         if len(email) < 4:
             flash("Email must be at least 4 characters.", category= 'error')
         elif len(first_name) < 2 or len(last_name) < 2:
