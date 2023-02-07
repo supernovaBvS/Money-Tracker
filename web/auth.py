056706a1ef3afbdff11568f3f644d8cb226cbcef
supernovaBvS <kongson14@gmail.com>
2023-02-07 15:22:47 +0800

    check if user login or not

--- web/auth.py
+++ web/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -16,17 +17,21 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Invalid password, try again!', category='error')
         else:
             flash('Email does not exist, try again!', category='error')
-    return render_template("login.html", booleans=True)
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("logout.html", user= "You",text= "have logout successfully!")
+    logout_user()
+    return redirect(url_for('auth.login'))
+    # return render_template("logout.html", user= "You",text= "have logout successfully!")
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -57,4 +62,4 @@ def sign_up():
             return redirect(url_for('views.home'))
 
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
